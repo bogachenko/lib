b9c6c977a21ec4c497f8d16180e5991caa1f83da
bogachenko <bogachenkove@gmail.com>
2020-01-27 21:11:35 +1100

    Update

--- linux/setupArch.sh
+++ linux/setupArch.sh
@@ -4,11 +4,65 @@
 # Author: Bogachenko Vyacheslav
 # Mail: bogachenkove@gmail.com
 
-hostname='bogachenko'
-username='bogachenko'
+loadkeys ru
+setfont cyr-sun16
+
+echo 'Синхронизация системных часов'
+timedatectl set-ntp true
+
+echo 'Cоздание разделов'
+(
+  echo o;
+  echo n;
+  echo p;
+  echo 1;
+  echo;
+  echo +100M;
+  echo n;
+  echo p;
+  echo 2;
+  echo;
+  echo +30G;
+  echo n;
+  echo p;
+  echo 3;
+  echo;
+  echo +8192M;
+  echo n;
+  echo p;
+  echo;
+  echo;
+  echo a;
+  echo 1;
+  echo w;
+) | fdisk /dev/sda
+
+echo 'Ваша разметка диска'
+fdisk -l
+
+echo 'Форматирование дисков'
+mkfs.ext2  /dev/sda1 -L boot
+mkfs.ext4  /dev/sda2 -L root
+mkswap /dev/sda3 -L swap
+mkfs.ext4  /dev/sda4 -L home
+
+echo 'Монтирование дисков'
+mount /dev/sda2 /mnt
+mkdir /mnt/{boot,home}
+mount /dev/sda1 /mnt/boot
+swapon /dev/sda3
+mount /dev/sda4 /mnt/home
+
+echo 'Установка основных пакетов'
+pacstrap /mnt base base-devel linux linux-firmware vim dhcpcd netctl
+
+echo 'Настройка системы'
+genfstab -pU /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
 
 echo 'Прописываем имя компьютера'
-echo $hostname > /etc/hostname
+echo bogachenko > /etc/hostname
 
 echo 'Добавляем русский и английский язык для системы'
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
@@ -39,13 +93,13 @@ echo 'Ставим программу для Wi-fi'
 pacman -S dialog wpa_supplicant --noconfirm 
 
 echo 'Добавляем пользователя'
-useradd -m -g users -G wheel -s /bin/bash $username
+useradd -m -g users -G wheel -s /bin/bash bogachenko
 
 echo 'Создаем root пароль'
 passwd
 
 echo 'Устанавливаем пароль пользователя'
-passwd $username
+passwd bogachenko
 
 echo 'Устанавливаем SUDO'
 echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
