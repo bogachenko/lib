c350683b22ea6a9c45fdd40154bb0f71373e6732
bogachenko <bogachenkove@gmail.com>
2020-03-24 15:55:39 +1100

    Update thunderbird-user.js

--- mozilla/thunderbird-user.js
+++ mozilla/thunderbird-user.js
@@ -340,3 +340,247 @@ user_pref("media.navigator.video.enabled", false);
 //		TRUE = Не воспроизводить
 //		FALSE = Воспроизводить
 user_pref("media.block-autoplay-until-in-foreground", true);
+
+// Activate the plugin by clicking
+// Активация плагина по клику
+user_pref("plugins.click_to_play", true);
+
+// Scanning Plugin ID (PLID) in directories defined in the Windows registry
+// Сканирование Plugin ID (PLID) в каталогах, определенных в реестре Windows
+user_pref("plugin.scan.plid.all", false);
+
+// OpenH264 codec (Gecko Media Plugins)
+// Кодек OpenH264 (Gecko Медиа Плагины)
+user_pref("media.gmp-provider.enabled", false);
+user_pref("media.gmp-gmpopenh264.enabled", false);
+user_pref("media.gmp-manager.cert.checkAttributes", false);
+user_pref("media.gmp.trial-create.enabled", false);
+user_pref("media.gmp-widevinecdm.enabled", false);
+user_pref("media.gmp-widevinecdm.visible", false);
+user_pref("media.gmp-gmpopenh264.visible", false);
+user_pref("media.gmp-manager.url", false);
+
+// Digital rights management (DRM)
+// Управление цифровыми правами (DRM)
+user_pref("media.eme.enabled", false);
+
+// Preload a page that the browser considers to be a logical next page
+// Предварительная загрузка страницы, которую браузер считает логической следующей страницей
+user_pref("network.prefetch-next", false);
+
+// Pre-query DNS for all links on the active page
+// [NOTE] For HTTP and HTTPS-protected pages. This feature allows the browser in the background to determine DNS for various web content in order to speed up subsequent downloads (for links, graphics, CSS, JavaScript, etc.).
+// Предварительный запрос DNS для всех ссылок на активной странице
+// [ЗАМЕТКА] Для HTTP и HTTPS-защищенных страниц. Эта функция позволяет браузеру в фоновом режиме определять DNS для различного веб-контента с целью ускорения последующей загрузки (для ссылок, графики, CSS, JavaScript и т.п.).
+user_pref("network.dns.disablePrefetch", true);
+user_pref("network.dns.disablePrefetchFromHTTPS", true);
+
+// Sending ping
+// Sending data about clicking on a link to the addresses specified in the "a" tag ("ping" attribute). HTML5 allows you to use this attribute for an element called ping. This function sends data by the URI defined in the hyperlink (if the user clicks on it). This allows you to track activity, user preferences and collect statistics about them.
+// Отправка пинга
+// Отправка данных о нажатии на ссылку адресам, указанным в тэге "a" (атрибут "ping"). HTML5 позволяет использовать этот атрибут для элемента под названием ping. Эта функция отсылает данные по URI, определенному в гиперссылке (если на нее нажимает пользователь). Это позволяет отслеживать активность, предпочтения пользователей и собирать статистику о них.
+user_pref("browser.send_pings", false);
+user_pref("browser.send_pings.max_per_link", 0);
+user_pref("browser.send_pings.require_same_host", true);
+
+// Captive portal detection
+// Обнаружение портала авторизации
+user_pref("captivedetect.canonicalURL", "");
+user_pref("network.captive-portal-service.enabled", false);
+user_pref("network.connectivity-service.enabled", false);
+
+// Cache API (Cache storage)
+// [NOTE] It is a repository on the users computer where scripts can store information. It is part of the "Service workers" specification, but can be used without them.
+// API Кэша (Хранилище кэша)
+// [ЗАМЕТКА] Это хранилище на компьютере пользователя, куда скрипты могут складывать информацию. Оно является частью спецификации "Service workers", но может быть использовано и без них.
+user_pref("dom.caches.enabled", false);
+
+// Web notifications
+// [NOTE] Data is sent to the site(s) not currently open in the browser.
+// Веб-уведомления
+// [ЗАМЕТКА] Данные отсылаются на сайт(ы) не открытые в данный момент в браузере.
+user_pref("dom.webnotifications.enabled", false);
+user_pref("dom.webnotifications.serviceworker.enabled", false);
+user_pref("dom.push.enabled", false);
+user_pref("dom.push.connection.enabled", false);
+user_pref("dom.push.serverURL", "");
+
+// Disk cache
+// Кэш-диска
+user_pref("browser.cache.disk.capacity", 0);
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.disk.smart_size.enabled", false);
+user_pref("browser.cache.disk.smart_size.first_run", false);
+user_pref("browser.cache.frecency_experiment", -1);
+user_pref("browser.cache.disk_cache_ssl", false);
+
+// Fastback cache
+//		0 = Disable
+//		-1 = Auto
+// Кэш Fastback
+//		0 = Отключить
+//		-1 = Автоматически
+user_pref("browser.sessionhistory.max_total_viewers", 0);
+
+// Offline cache
+// Автономный кэш
+user_pref("browser.cache.offline.enable", false);
+user_pref("browser.cache.offline.insecure.enable", false);
+
+// Request to use the offline cache
+// Запрос на использование автономного кеша
+user_pref("offline-apps.allow_by_default", false);
+
+// Cookies
+// A Cookie is a piece of data (a small text file) sent by the server and stored in the browser. When the user reloads the same page, the browser sends the cookie back to the server and notifies it of the previous activity. Cookies store information such as passwords, logins, session data, credit card data, email addresses, and record various user activity and other statistics.
+// [WARNING] Cookies are actively used by many web resources in the process of monitoring and tracking user activity. Theft and analysis of cookies is a potentially dangerous and serious attack, leading to global leaks of user data and use.
+//		0 = Accept cookies and site data
+//		1 = Block third-party cookies
+//		2 = Block all cookies
+//		3 = Block cookies from sites not visited
+// Куки
+// Cookie - фрагмент данных (небольшой текстовый файл), отправляемый сервером и сохраняющийся в браузере. Когда пользователь вновь загружает ту же самую страницу, браузер отсылает cookie назад на сервер и уведомляет его о предыдущей активности. Cookie хранят такую информацию, как пароли, логины, сеансовые данные, данные кредитных карт, электронные почтовые адреса и записывают различную пользовательскую активность и прочую статистику.
+// [ВНИМАНИЕ] Cookie активно используются многими веб-ресурсами в процессе контроля и слежки за пользовательской активностью. Похищение и анализ cookie является потенциально опасной и серьезной атакой, приводящей к глобальным утечкам пользовательских данных и их использованию.
+//		0 = Принимать куки и данные сайтов
+//		1 = Блокировать стороннии куки
+//		2 = Блокировать все куки
+//		3 = Блокировать куки с не посещенных сайтов
+user_pref("network.cookie.cookieBehavior", 0);
+
+// Third-party cookies are set for the session only
+// Сторонние файлы куки устанавливаются только для сеанса
+user_pref("network.cookie.thirdparty.sessionOnly", true);
+user_pref("network.cookie.thirdparty.nonsecureSessionOnly", true);
+
+// Сookie lifetime policy
+//		0 = Save them before expiration
+//		2 = Save them until close Firefox
+// Политика жизни куки
+//		0 = Сохранять их до истечения срока действия
+//		2 = Сохранять их до закрытия Firefox
+user_pref("network.cookie.lifetimePolicy", 2);
+
+// Storage API
+// [NOTE] Disables another way for sites to store their data on the users personal computer.
+// API Хранилища
+// [ЗАМЕТКА] Отключает еще один способ для сайтов хранить свои данные на персональном компьютере пользователя.
+user_pref("dom.storageManager.enabled", false);
+
+// Limit events that can trigger a pop-up
+// Ограничения событий, которые могут вызвать всплывающее окно
+user_pref("dom.popup_allowed_events", "click dblclick");
+
+// Special services (accessibility support services)
+// Специальные услуги (службы поддержки доступности)
+user_pref("accessibility.force_disabled", 1);
+
+// Additional analytics sent to the web server
+// [NOTE] HTTP-data leaking from UserAgent to the server, especially when leaving the page.
+// Дополнительная аналитика, отправленная на веб-сервер
+// [ЗАМЕТКА] HTTP-данные утекающих от UserAgent на сервер, особенно при покидании страницы.
+user_pref("beacon.enabled", false);
+
+// Deleting temporary files opened by an external application
+// Удаление временных файлов, открытые внешним приложением
+user_pref("browser.helperApps.deleteTempFileOnExit", true);
+
+// Web speech
+// Веб-речь
+user_pref("media.webspeech.synth.enabled", false);
+user_pref("media.webspeech.recognition.enable", false);
+
+// Collecting HTML video statistics
+// Сбор HTML-видеостатистики
+user_pref("media.video_stats.enabled", false);
+
+// Autocomplete user name and password
+// Aвтоматическое заполнение имени пользователя и пароля
+user_pref("signon.autofillForms", false);
+user_pref("signon.autofillForms.http", false);
+
+// Smooth scrolling
+// [NOTE] Recommended for brake applications, hardware failures, and problems with the video card.
+// Плавная прокрутка
+// [ЗАМЕТКА] Рекомендовано при торможениях, аппаратных сбоях и проблемах с видеокартой.
+user_pref("general.smoothScroll", false);
+
+// Automatic update themes
+// Автоматическое обновление тем
+user_pref("lightweightThemes.update.enabled", false);
+
+// Path to save files
+//		0 = Desktop
+//		1 = Downloads
+//		2 = Last used folder
+// [NOTE] On Android, when you select "Save to desktop", downloading will not work at all
+// Путь для сохранения файлов
+//		0 = Рабочий стол
+//		1 = Загрузки
+//		2 = Последняя используемая папка
+// [ЗАМЕТКА] На Android при выборе "Сохранять на рабочий стол" загрузка вообще работать не будет
+user_pref("browser.download.folderList", 1);
+
+// Adding downloads to the list of recent system documents
+// Добавление загрузок в список последних документов системы
+user_pref("browser.download.manager.addToRecentDocs", false);
+
+// Show the "Open with" dialog box in the download window
+// Показ диалогового окна "Открыть при помощи" в окне загрузки
+user_pref("browser.download.forbid_open_with", true);
+
+// Predictor
+// [NOTE] This function creates the simplest connections to the server. When the cursor is over a particular link, Firefox tries to anticipate the users actions on the page in order to increase the performance and speed of content processing on the page.
+// Предсказатель
+// [ЗАМЕТКА] Это функция создает простейшие соединения с сервером. Когда курсор находится над определенной ссылкой, Firefox пытается предугадать действия пользователя на странице с целью увеличения производительность и скорости обработки контента на странице.
+user_pref("network.predictor.enabled", false);
+user_pref("network.predictor.enable-hover-on-ssl", false);
+user_pref("network.predictor.enable-prefetch", false);
+
+// WebExtension restrictions for specific Mozilla domains
+// Ограничения WebExtension для определенных доменов Mozilla
+user_pref("extensions.webextensions.restrictedDomains", "");
+
+// Install add-ons without signatures and display a request to install add-ons in the browser
+// Установка дополнений без подписей и вывод запроса на установку дополнений в браузер
+// user_pref("xpinstall.whitelist.required", true);
+user_pref("xpinstall.signatures.required", false);
+
+// Animated gradient that runs through the tab after the page is fully loaded
+// Анимированный градиент, который пробегает по вкладке после полной загрузки страницы
+user_pref("toolkit.cosmeticAnimations.enabled", false);
+
+// Auto scroll
+// Автоматическая прокрутка
+user_pref("general.autoScroll", false);
+
+// Reader view
+// [NOTE] Load reduction.
+// Просмотр для чтения
+// [ЗАМЕТКА] Снижение нагрузки.
+user_pref("reader.parse-on-load.enabled", false);
+
+// Text reader function in Reader view
+// Функция зачитывания текста в Reader view
+user_pref("narrate.enabled", false);
+
+// WebGL debug info being available to websites
+// Информация об отладке WebGL доступна для веб-сайтов
+user_pref("webgl.enable-debug-renderer-info", false);
+
+// Service Workers
+// "Service workers" essentially act as proxy servers that sit between web apps, and the browser and network, are event driven, and can control the web page/site it is associated with, intercepting and modifying navigation and resource requests, and caching resources.
+// [NOTE] This heavily loads the system resources and takes up most of the RAM.
+// Service Workers
+// "Service workers" по существу действуют как прокси-серверы, которые находятся между веб-приложениями, браузером и сетью, управляются событиями и могут управлять веб-страницей или сайтом, с которыми они связаны, перехватывать и изменять запросы навигации и ресурсов, а также кэшировать ресурсы.
+// [ЗАМЕТКА] Это сильно нагружает ресурсы системы и занимает большую часть оперативной памяти.
+user_pref("dom.serviceWorkers.enabled", false);
+
+// Detection of connection/disconnection of media devices in the system
+// Обнаружение подключения/отключения медиа-устройств в системе
+user_pref("media.ondevicechange.enabled", false);
+
+// Sites with autocomplete="off" attribute
+// [NOTE] This function allows you to save the entered passwords in the password manager, even if the site owner has set the autocomplete attribute to "off".
+// Сайты с атрибутом автозаполнение "off"
+// [ЗАМЕТКА] Данная функция позволяет сохранять введенные пароли в менеджере паролей, даже если владелец сайта установил атрибуту автозаполнение значение "off".
+user_pref("signon.storeWhenAutocompleteOff", true)
