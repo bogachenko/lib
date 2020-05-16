32d114e312a86d81b98a161d95ae7e4fda1abda5
bogachenko <bogachenkove@gmail.com>
2020-05-16 21:55:58 +1100

    Update files

--- scripts/hosts.bat
+++ scripts/hosts.bat
@@ -1,2 +1,6 @@
-@echo off
-powershell "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/bogachenko/filterlist/personal/hosts','hosts')"
+@ECHO OFF
+TITLE bogachenko's Personal Filter Hosts
+ECHO Hosts file update script
+POWERSHELL "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/bogachenko/filterlist/personal/hosts','hosts')"
+
+exit
