15ff0d1e3dd43384f75759c9d28fbf66d56a1cc4
bogachenko <bogachenkove@gmail.com>
2020-05-17 18:06:55 +1100

    Update hosts.bat

--- scripts/hosts.bat
+++ scripts/hosts.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 TITLE bogachenko's Personal Filter Hosts
 ECHO Hosts file update script
-POWERSHELL "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/bogachenko/filterlist/personal/hosts','hosts')"
-
+POWERSHELL "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/bogachenko/filterlist/personal/hosts','%USERPROFILE%\AppData\Local\Temp\bogachenko\hosts')"
+move /y %USERPROFILE%\AppData\Local\Temp\bogachenko\hosts %WINDIR%\System32\drivers\etc\
 exit
