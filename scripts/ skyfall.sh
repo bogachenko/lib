fb3c6d372ab0e333f948247969ce37997c20c2c8
bogachenko <bogachenkove@gmail.com>
2020-06-01 10:13:59 +1100

    Update and rename bomjaro.sh to  skyfall.sh

---
+++ scripts/ skyfall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Skyfall: Automatic configuration of Manjaro.
+# Author: Bogachenko Vyacheslav
+# Mail: bogachenkove@gmail.com
+
+echo "Checking network connection."
+echo "Please wait..."
+ping google.com -c 1 >/dev/null 2>&1
+
+if [ $? != "0" ]
+	echo "Good, you have an internet connection!"
+then
+    echo "Network error, please check network connection!"
+    pause
+fi
+sleep 5
+su
+pacman -Syyuu
+pacman -S firefox-i18n-ru vim vlc steam-native-runtime steam opera opera-ffmpeg-codecs gimp
+systemctl enable bumblebeed
+gpasswd -a $USER bumblebee
+rm -rf /home/bogachenko/.mozilla/
+rm /etc/hosts
+wget -P /etc/ https://raw.githubusercontent.com/bogachenko/filterlist/personal/hosts
