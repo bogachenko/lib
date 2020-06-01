b4e7c0a0a2240e9c03eaca75c25c41c36348cd36
bogachenko <bogachenkove@gmail.com>
2020-06-01 19:27:59 +1100

    Update skyfall.sh

--- scripts/skyfall.sh
+++ scripts/skyfall.sh
@@ -24,3 +24,13 @@ gpasswd -a $USER bumblebee
 rm -rf /home/bogachenko/.mozilla/
 rm /etc/hosts
 wget -P /etc/ https://raw.githubusercontent.com/bogachenko/filterlist/personal/hosts
+sudo pacman -S --needed base-devel git wget yajl
+git clone https://aur.archlinux.org/package-query.git
+cd package-query/
+makepkg -si
+cd ..
+git clone https://aur.archlinux.org/yaourt.git
+cd yaourt/
+makepkg -si
+cd ..
+sudo rm -dR yaourt/ package-query/
