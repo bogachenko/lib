c63a72433e766c130b24a6c1b2132d1c88b8879b
bogachenko <bogachenkove@gmail.com>
2020-06-03 23:12:55 +1100

    Update bomjaro.sh

--- scripts/bomjaro.sh
+++ scripts/bomjaro.sh
@@ -19,13 +19,13 @@ sleep 5
 su
 pacman-mirrors --fasttrack
 pacman -Syyuu
-pacman -S firefox-i18n-ru vim vlc steam-native-runtime steam opera opera-ffmpeg-codecs gimp ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-ubuntu-font-family jre-openjdk jdk-openjdk jre-openjdk-headless chromium capitaine-cursors discord telegram-desktop zsh thunderbird-i18n-ru thunderbird gparted wine wine-gecko wine-mono virtualbox
+pacman -S firefox-i18n-ru vim vlc steam-native-runtime steam opera opera-ffmpeg-codecs gimp ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-ubuntu-font-family jre-openjdk jdk-openjdk jre-openjdk-headless chromium capitaine-cursors discord telegram-desktop zsh thunderbird-i18n-ru thunderbird gparted wine wine-gecko wine-mono virtualbox qbittorrent
 chsh -s /bin/zsh
 cd /home/$USER
 rm -rf .bash_history .bash_logout .bash_profile .bashrc
 systemctl enable bumblebeed
 systemctl restart systemd-binfmt
-gpasswd -a $USER bumblebee
+gpasswd -a $USER bumblebee vboxusers
 rm -rf /home/$USER/.mozilla/
 cd /home/$USER/.mozilla/firefox/
 mkdir $USER
@@ -47,6 +47,10 @@ exit
 sudo chsh -s /bin/zsh
 yaourt -S ttf-ms-fonts vscodium-bin github-desktop-bin
 ln -s /usr/share/icons/capitaine-cursors-light/cursors ~/.icons/default/cursors
+cd /home/$USER
+mkdir wine
+WINEARCH=win32 WINEPREFIX=~/wine/win32 winecfg
+WINEPREFIX=~/wine/win64 winecfg
 sudo pacman -Syyuu
 yaourt -Syu
 sudo paccache -r
