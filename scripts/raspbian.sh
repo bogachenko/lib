7c3983b59ca22ef2ed16c9236eaadae0cc205fe7
bogachenko <bogachenkove@gmail.com>
2021-04-10 10:47:09 +1100

    Update and rename respbian.sh to raspbian.sh

---
+++ scripts/raspbian.sh
@@ -0,0 +1,6 @@
+// Raspberry Pi OS
+
+sudo apt update
+sudo apt upgrade
+clear
+sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr
--- scripts/respbian.sh
+++
@@ -1,4 +0,0 @@
-sudo apt update
-sudo apt upgrade
-clear
-sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc
