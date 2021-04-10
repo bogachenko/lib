c6585780d44bc2d6b82fe18917f46f5b96828c28
bogachenko <bogachenkove@gmail.com>
2021-04-10 15:04:46 +1100

    Update raspbian.sh

--- scripts/raspbian.sh
+++ scripts/raspbian.sh
@@ -5,7 +5,8 @@ sudo raspi-config
 clear
 sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr vlc neofetch firefox-esr
 sudo apt install xrdp
-## nano /etc/resolv.conf
-sudo purge chromium-browser
+nano /etc/resolv.conf
+## sudo purge chromium-browser
+curl -sSL https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh
 clear
 sudo apt autoremove
