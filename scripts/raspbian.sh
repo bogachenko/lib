f86ebf1c08976dddb40a275f70b8940cb293723f
bogachenko <bogachenkove@gmail.com>
2021-04-10 14:04:30 +1100

    Update raspbian.sh

--- scripts/raspbian.sh
+++ scripts/raspbian.sh
@@ -1,9 +1,11 @@
-// Raspberry Pi OS
+## Raspberry Pi OS
 
 sudo apt update && sudo apt upgrade
 sudo raspi-config
 clear
 sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr vlc neofetch firefox-esr
+sudo apt install xrdp
+## nano /etc/resolv.conf
 sudo purge chromium-browser
 clear
 sudo apt autoremove
