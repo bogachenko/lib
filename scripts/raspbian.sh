10776c534143129867765c834ebe22c1420aa6bd
bogachenko <bogachenkove@gmail.com>
2021-04-10 12:02:31 +1100

    Update raspbian.sh

--- scripts/raspbian.sh
+++ scripts/raspbian.sh
@@ -3,4 +3,6 @@
 sudo apt update
 sudo apt upgrade
 clear
-sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr
+sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr vlc neofetch lxqt sddm
+clear
+sudo raspi-config
