d7cef606306f7710d3b73dd621e92fea8e95c2be
bogachenko <bogachenkove@gmail.com>
2021-04-10 16:24:04 +1100

    Update raspbian.sh

--- scripts/raspbian.sh
+++ scripts/raspbian.sh
@@ -1,12 +1,13 @@
 ## Raspberry Pi OS
 
+sudo systemctl enable ssh && sudo systemctl start ssh
 sudo apt update && sudo apt upgrade
 sudo raspi-config
 clear
-sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr vlc neofetch firefox-esr
-sudo apt install xrdp
-nano /etc/resolv.conf
-## sudo purge chromium-browser
+sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr vlc neofetch firefox-esr firefox-esr-l10n-ru
 curl -sSL https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh
+sudo apt install xrdp
+sudo vim /etc/resolv.conf
+## sudo purge chromium-browser
 clear
 sudo apt autoremove
