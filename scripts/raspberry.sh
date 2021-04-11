98e8955f5899fdc97bf43d6aae457baad2d27169
bogachenko <bogachenkove@gmail.com>
2021-04-11 16:14:54 +1100

    Update and rename raspbian.sh to raspberry.sh

--- scripts/raspberry.sh
+++ scripts/raspberry.sh
@@ -1,13 +1,13 @@
-## Raspberry Pi OS
+## Raspberry Pi
 
 sudo systemctl enable ssh && sudo systemctl start ssh
 sudo apt update && sudo apt upgrade
 sudo raspi-config
 clear
-sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr vlc neofetch firefox-esr firefox-esr-l10n-ru
+sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr vlc neofetch firefox-esr firefox-esr-l10n-ru hostapd dnsmasq
 curl -sSL https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh
 sudo apt install xrdp
 sudo vim /etc/resolv.conf
-## sudo purge chromium-browser
+sudo apt purge chromium-browser chromium-codecs-ffmpeg-extra
 clear
 sudo apt autoremove
