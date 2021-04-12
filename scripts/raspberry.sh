31b9aefe09b4cfc1fad9ba68131101ee2ac902f2
bogachenko <bogachenkove@gmail.com>
2021-04-12 12:26:37 +1100

    Update raspberry.sh

--- scripts/raspberry.sh
+++ scripts/raspberry.sh
@@ -4,10 +4,9 @@ sudo systemctl enable ssh && sudo systemctl start ssh
 sudo apt update && sudo apt upgrade
 sudo raspi-config
 clear
-sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr vlc neofetch firefox-esr firefox-esr-l10n-ru hostapd dnsmasq
+sudo apt --no-install-suggests --no-install-recommends install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra git tor vim ssh mc htop vlc neofetch hostapd dnsmasq
 curl -sSL https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh
-sudo apt install -y xrdp fonts-noto
+sudo apt install -y xrdp fonts-noto ttf-mscorefonts-installer
 sudo vim /etc/resolv.conf
-sudo apt purge chromium-browser chromium-codecs-ffmpeg-extra
 clear
 sudo apt autoremove
