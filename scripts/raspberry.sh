4e63ba973bcd6bc8d78a229d9580f7b9e5c38fa6
bogachenko <bogachenkove@gmail.com>
2021-04-12 17:18:09 +1100

    Update raspberry.sh

--- scripts/raspberry.sh
+++ scripts/raspberry.sh
@@ -10,6 +10,9 @@ sudo systemctl enable ssh.service && sudo systemctl start ssh.service
 # Updating repository data and installing updates.
 sudo apt update && sudo apt upgrade
 
+# Adding the Kali repository.
+sudo sh -c "# deb http://http.kali.org/kali kali-rolling main non-free contrib' >> /etc/apt/sources.list"
+
 # Running the standard config file for the Raspberry Pi.
 sudo raspi-config
 
@@ -18,14 +21,13 @@ curl -sSL https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scrip
 clear
 
 # Installing main packages.
-sudo apt --no-install-suggests --no-install-recommends install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra git tor vim mc htop vlc neofetch hostapd dnsmasq resolvconf
+sudo apt --no-install-suggests --no-install-recommends install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra git tor vim mc htop vlc neofetch hostapd dnsmasq resolvconf privoxy
 sudo apt install -y xrdp fonts-noto ttf-mscorefonts-installer
 
 # Enabling daemons and starting them for my main packages.
-sudo systemctl enable resolvconf.service
-sudo systemctl start resolvconf.service
-sudo systemctl start tor.service
-sudo systemctl enable tor.service
+sudo systemctl enable resolvconf.service && sudo systemctl start resolvconf.service
+sudo systemctl start tor.service && sudo systemctl enable tor.service
+sudo systemctl start privoxy.service && sudo systemctl enable privoxy.service
 
 # Setting up static DNS (which I gave for the Raspberry Pi in my router settings) for Adguard Home.
 cat > /etc/resolvconf/resolv.conf.d/head <<EOF 
