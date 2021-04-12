42a95fd394a752971d99e16e9538767ab512ed02
bogachenko <bogachenkove@gmail.com>
2021-04-12 12:04:00 +1100

    Update raspberry.sh

--- scripts/raspberry.sh
+++ scripts/raspberry.sh
@@ -6,7 +6,7 @@ sudo raspi-config
 clear
 sudo apt --no-install-suggests --no-install-recommends install git tor vim ssh mc htop firefox-esr vlc neofetch firefox-esr firefox-esr-l10n-ru hostapd dnsmasq
 curl -sSL https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh
-sudo apt install xrdp
+sudo apt install -y xrdp fonts-noto
 sudo vim /etc/resolv.conf
 sudo apt purge chromium-browser chromium-codecs-ffmpeg-extra
 clear
