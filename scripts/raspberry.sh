149b414477200552e09394abee9570806d65be36
bogachenko <bogachenkove@gmail.com>
2021-04-12 16:39:16 +1100

    Update raspberry.sh

--- scripts/raspberry.sh
+++ scripts/raspberry.sh
@@ -17,18 +17,23 @@ sudo raspi-config
 curl -sSL https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh
 clear
 
-# Installing core packages
+# Installing main packages.
 sudo apt --no-install-suggests --no-install-recommends install chromium-browser chromium-browser-l10n chromium-codecs-ffmpeg-extra git tor vim mc htop vlc neofetch hostapd dnsmasq resolvconf
 sudo apt install -y xrdp fonts-noto ttf-mscorefonts-installer
 
+# Enabling daemons and starting them for my main packages.
 sudo systemctl enable resolvconf.service
 sudo systemctl start resolvconf.service
+sudo systemctl start tor.service
+sudo systemctl enable tor.service
 
+# Setting up static DNS (which I gave for the Raspberry Pi in my router settings) for Adguard Home.
 cat > /etc/resolvconf/resolv.conf.d/head <<EOF 
 nameserver 192.168.0.104
 nameserver 192.168.0.102
 EOF
 
+# Updating configuration files for DNS and setting it to default on reboot.
 sudo resolvconf --enable-updates
 sudo resolvconf -u
 cat /etc/resolv.conf
