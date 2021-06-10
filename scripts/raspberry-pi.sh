78d68b0909d5e012d1e3979dda08c8087916d84b
bogachenko <bogachenkove@gmail.com>
2021-06-10 20:53:04 +1100

    Update raspberry-pi.sh

--- scripts/raspberry-pi.sh
+++ scripts/raspberry-pi.sh
@@ -38,7 +38,7 @@ pacman-mirrors
 pacman -Syyuu
 
 # Installing main packages.
-pacman -S --needed zsh git vim htop neofetch net-tools tor privoxy cmake pkgconf make iw base-devel wget ttf-ubuntu-font-family ttf-dejavu ttf-liberation netctl gparted openresolv xorg-drivers xorg-server ranger code firefox-i18n-ru firefox xorg-xinit jack2 noto-fonts noto-fonts-emoji sddm dmenu i3-wm scrot xorg-xsetroot i3status gvfs dhclient alsa-plugins alsa-utils pulseaudio nyx vlc noto-fonts-cjk xorg-xrdb speedtest-cli xdg-user-dirs atril gtk2 gtk3 gtk4 dhcpcd xdg-utils xautolock pavucontrol hostapd xorg-apps dnsmasq rxvt-unicode unzip i3lock ppp bluez bluez-untils ttf-fira-code ttf-fira-mono mathjax youtube-dl thunderbird qmmp pcmanfm-qt python2 python create_ap ttf-carlito ttf-caladea ttf-croscore libevent perl xorg-xclock xorg-xmodmap npm nodejs terminus-font
+pacman -S --needed zsh git vim htop neofetch net-tools tor privoxy cmake pkgconf make iw base-devel wget ttf-ubuntu-font-family ttf-dejavu ttf-liberation netctl gparted openresolv xorg-drivers xorg-server ranger code firefox-i18n-ru firefox xorg-xinit jack2 noto-fonts noto-fonts-emoji sddm dmenu i3-wm scrot xorg-xsetroot i3status gvfs dhclient alsa-plugins alsa-utils pulseaudio nyx vlc noto-fonts-cjk xorg-xrdb speedtest-cli xdg-user-dirs atril gtk2 gtk3 gtk4 dhcpcd xdg-utils xautolock pavucontrol hostapd xorg-apps dnsmasq rxvt-unicode unzip i3lock ppp bluez bluez-untils ttf-fira-code ttf-fira-mono mathjax youtube-dl thunderbird qmmp pcmanfm-qt python2 python create_ap ttf-carlito ttf-caladea ttf-croscore libevent perl xorg-xclock xorg-xmodmap npm nodejs terminus-font mesa mesa-demos
 
 # Automatic login.
 cat > /etc/sddm.conf <<EOF
@@ -109,7 +109,7 @@ sudo ./strap.sh
 sudo pacman -Syu
 
 # Installing main packages from the AUR repository.
-yaourt -S mocu-xcursor 3proxy peerflix
+yaourt -S mocu-xcursor peerflix
 
 # Installing main packages from the BlackArch repository.
 sudo pacman -S 42zip
@@ -174,6 +174,7 @@ alias ls='ls -la'
 alias reboot='sudo reboot'
 alias updxres='xrdb -merge ~/.Xresources'
 alias pac='pacman'
+alias pacyu='pacman -Syu'
 EOF
 sudo cat > /root/.zshrc <<EOF
 PROMPT="%F{9}%n%f%F{9}@%f%F{9}%m%f:%F{21}%~%f# "
@@ -181,10 +182,11 @@ export BROWSER="firefox"
 export EDITOR="vim"
 alias ls='ls -la'
 alias pac='pacman'
+alias pacyu='pacman -Syu'
 EOF
 
 # Removing debris.
-sudo rm -rf /root/.bash_history /root/.bashrc
+sudo rm -rf /root/.bash_history /root/.bashrc /root/.bash_logout ~/.bashrc ~/.bash_history ~/.bash_logout
 sudo pacman -Sc
 
 # Clear the console.
