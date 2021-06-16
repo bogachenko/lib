cab6719ef400ea5d611a57211c27e46512f47a1f
bogachenko <bogachenkove@gmail.com>
2021-06-16 17:03:27 +1100

    Update raspberry-pi.sh

--- scripts/raspberry-pi.sh
+++ scripts/raspberry-pi.sh
@@ -20,7 +20,7 @@ EOF
 locale-gen
 setfont ter-k16n
 localectl set-locale LANG="ru_RU.UTF-8"
-sudo cat > /etc/X11/xorg.conf.d/00-keyboard.conf <<EOF
+cat > /etc/X11/xorg.conf.d/00-keyboard.conf <<EOF
 Section "InputClass"
         Identifier "system-keyboard"
         MatchIsKeyboard "on"
@@ -32,13 +32,18 @@ EndSection
 EOF
 
 # Refresh mirrors for Pacman.
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+cat > /etc/pacman.d/mirrorlist <<EOF
+Server = https://mirror.yandex.ru/mirrors/manjaro/arm-stable/$repo/$arch
+Server = https://mirror.truenetwork.ru/mirrors/manjaro/arm-stable/$repo/$arch
+EOF
 pacman-mirrors
 
 # Updating repository data and installing updates.
 pacman -Syyuu
 
 # Installing main packages.
-pacman -S --needed zsh git vim htop neofetch net-tools tor privoxy cmake pkgconf make iw base-devel wget ttf-ubuntu-font-family ttf-dejavu ttf-liberation netctl gparted openresolv xorg-drivers xorg-server ranger code firefox-i18n-ru firefox xorg-xinit jack2 noto-fonts noto-fonts-emoji sddm dmenu i3-wm scrot xorg-xsetroot i3status gvfs dhclient alsa-plugins alsa-utils pulseaudio nyx vlc noto-fonts-cjk xorg-xrdb speedtest-cli xdg-user-dirs atril gtk2 gtk3 gtk4 dhcpcd xdg-utils xautolock pavucontrol hostapd xorg-apps dnsmasq rxvt-unicode unzip i3lock ppp bluez bluez-untils ttf-fira-code ttf-fira-mono mathjax youtube-dl thunderbird qmmp pcmanfm-qt python2 python create_ap ttf-carlito ttf-caladea ttf-croscore libevent perl xorg-xclock xorg-xmodmap npm nodejs terminus-font mesa mesa-demos papirus-icon-theme qt5ct
+pacman -S --needed zsh git vim htop neofetch net-tools tor privoxy cmake pkgconf make iw base-devel wget ttf-ubuntu-font-family ttf-dejavu ttf-liberation netctl gparted openresolv xorg-drivers xorg-server ranger code firefox-i18n-ru firefox xorg-xinit jack2 noto-fonts noto-fonts-emoji sddm dmenu i3-wm scrot xorg-xsetroot i3status gvfs dhclient alsa-plugins alsa-utils pulseaudio nyx vlc noto-fonts-cjk xorg-xrdb speedtest-cli xdg-user-dirs atril gtk2 gtk3 gtk4 dhcpcd xdg-utils xautolock pavucontrol hostapd xorg-apps dnsmasq rxvt-unicode unzip i3lock ppp bluez bluez-untils ttf-fira-code ttf-fira-mono mathjax youtube-dl thunderbird qmmp pcmanfm-qt python2 python create_ap ttf-carlito ttf-caladea ttf-croscore libevent perl xorg-xclock xorg-xmodmap npm nodejs terminus-font mesa mesa-demos papirus-icon-theme qt5ct pwgen imagemagick
 
 # Installing the theme.
 cat > /usr/share/gtk-2.0/gtkrc <<EOF
