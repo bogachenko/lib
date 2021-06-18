f0a984da961c5009d63c9a54795a947dd820a232
bogachenko <bogachenkove@gmail.com>
2021-06-18 18:02:44 +1100

    Update raspberry-pi.sh

--- scripts/raspberry-pi.sh
+++ scripts/raspberry-pi.sh
@@ -338,6 +338,12 @@ mode "resize" {
         bindsym $mod+r mode "default"
 }
 bindsym $mod+r mode "resize"
+client.focused          #4C7899 #285577 #FFFFFF #2E9EF4   #285577
+client.focused_inactive #333333 #5F676A #FFFFFF #484E50   #5F676A
+client.unfocused        #333333 #222222 #888888 #292D2E   #222222
+client.urgent           #2F343A #900000 #FFFFFF #900000   #900000
+client.placeholder      #000000 #0C0C0C #FFFFFF #000000   #0C0C0C
+client.background       #FFFFFF
 bar {
         status_command i3status
 colors {
