4c4db9e5d42aef6ba8293e3ccef4cc976e234949
bogachenko <bogachenkove@gmail.com>
2021-06-23 00:46:21 +1100

    Update raspberry-pi.sh

--- scripts/raspberry-pi.sh
+++ scripts/raspberry-pi.sh
@@ -43,7 +43,7 @@ pacman-mirrors --country Russia
 pacman -Syyuu
 
 # Installing main packages.
-pacman -S --needed zsh git vim htop neofetch net-tools tor privoxy cmake pkgconf make iw base-devel wget ttf-ubuntu-font-family ttf-dejavu ttf-liberation netctl gparted openresolv xorg-drivers xorg-server ranger code firefox-i18n-ru firefox xorg-xinit jack2 noto-fonts noto-fonts-emoji sddm dmenu i3-wm scrot xorg-xsetroot i3status gvfs dhclient alsa-plugins alsa-utils pulseaudio nyx vlc noto-fonts-cjk xorg-xrdb speedtest-cli xdg-user-dirs gtk2 gtk3 gtk4 dhcpcd xdg-utils xautolock pavucontrol hostapd xorg-apps dnsmasq rxvt-unicode unzip i3lock ppp bluez bluez-untils ttf-fira-code ttf-fira-mono mathjax youtube-dl pcmanfm-qt python2 python create_ap ttf-carlito ttf-caladea ttf-croscore libevent perl xorg-xclock xorg-xmodmap npm nodejs terminus-font mesa mesa-demos qt5ct pwgen imagemagick dunst libjpeg-turbo capitaine-cursors breeze-icons chromium yajl zip unrar p7zip bzip2 lrzip lz4 lzop xz zstd arj lhasa lxqt-archiver onboard
+pacman -S --needed zsh git vim htop neofetch net-tools tor privoxy cmake pkgconf make iw base-devel wget ttf-ubuntu-font-family ttf-dejavu ttf-liberation netctl gparted openresolv xorg-drivers xorg-server ranger code firefox-i18n-ru firefox xorg-xinit jack2 noto-fonts noto-fonts-emoji sddm dmenu i3-wm scrot xorg-xsetroot i3status gvfs dhclient alsa-plugins alsa-utils pulseaudio nyx vlc noto-fonts-cjk xorg-xrdb speedtest-cli xdg-user-dirs gtk2 gtk3 gtk4 dhcpcd xdg-utils xautolock pavucontrol hostapd xorg-apps dnsmasq rxvt-unicode unzip i3lock ppp bluez bluez-untils ttf-fira-code ttf-fira-mono mathjax youtube-dl pcmanfm-qt python2 python create_ap ttf-carlito ttf-caladea ttf-croscore libevent perl xorg-xclock xorg-xmodmap npm nodejs terminus-font mesa mesa-demos qt5ct pwgen imagemagick dunst libjpeg-turbo capitaine-cursors breeze-icons chromium yajl zip unrar p7zip bzip2 lrzip lz4 lzop xz zstd arj lhasa lxqt-archiver onboard uget
 
 # Setting a parameter for QT.
 cat > /etc/environment <<EOF
@@ -385,6 +385,7 @@ alias updsys='sudo pacman -Syu'
 alias vi='vim'
 alias c='clear'
 alias wifi-router='sudo create_ap wlan0 eth0 bogachenko N7GZiMD!2ZTaZWYj0mLV'
+alias code="code --user-data-dir /home/$TELLUSER/.config/Code/"
 
 EOF
 sudo cat > /root/.zshrc <<EOF
@@ -401,6 +402,7 @@ alias updsys='pacman -Syu'
 alias vi='vim'
 alias c='clear'
 alias wifi-router='create_ap wlan0 eth0 bogachenko N7GZiMD!2ZTaZWYj0mLV'
+alias code="code --user-data-dir /home/$TELLUSER/.config/Code/"
 
 EOF
 
