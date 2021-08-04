6f4ad55805d117c7746bf8ac755ab7a66e31633a
bogachenko <bogachenkove@gmail.com>
2021-08-04 13:01:22 +1100

    Update archlinux-x86-64.sh

--- scripts/archlinux-x86-64.sh
+++ scripts/archlinux-x86-64.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Arch Linux x86_64
 # Author: Bogachenko Vyacheslav <bogachenkove@gmail.com>
 
 TELLUSER=$(whoami)
@@ -58,22 +59,30 @@ pacman -Syyuu
 yaourt -Syua
 
 # Installing main packages.
-pacman -S --needed zsh git vim htop neofetch net-tools tor privoxy cmake pkgconf make iw base-devel wget ttf-ubuntu-font-family ttf-dejavu ttf-liberation netctl gparted openresolv xorg-drivers xorg-server ranger code firefox-i18n-ru firefox xorg-xinit jack2 noto-fonts noto-fonts-emoji scrot xorg-xsetroot gvfs dhclient alsa-plugins alsa-utils pulseaudio nyx vlc noto-fonts-cjk xorg-xrdb speedtest-cli xdg-user-dirs gtk2 gtk3 gtk4 dhcpcd xdg-utils xautolock hostapd xorg-apps dnsmasq rxvt-unicode unzip ppp bluez bluez-untils mathjax youtube-dl python2 python ttf-carlito ttf-caladea ttf-croscore libevent perl xorg-xclock xorg-xmodmap npm nodejs terminus-font mesa mesa-demos qt5ct pwgen imagemagick libjpeg-turbo capitaine-cursors breeze-icons chromium yajl zip unrar p7zip bzip2 lrzip lz4 lzop xz zstd arj lhasa onboard pulseaudio-bluetooth pulseaudio-equalizer phonon-qt5-vlc plasma-desktop kf5 kf5-aids plasma-wayland-session opera xf86-input-synaptics
+pacman -S --needed zsh git vim htop neofetch net-tools tor privoxy cmake pkgconf make iw base-devel wget ttf-ubuntu-font-family ttf-dejavu ttf-liberation netctl gparted openresolv xorg-drivers xorg-server ranger code firefox-i18n-ru firefox xorg-xinit jack2 noto-fonts noto-fonts-emoji scrot xorg-xsetroot dhclient alsa-plugins alsa-utils pulseaudio nyx vlc noto-fonts-cjk xorg-xrdb speedtest-cli gtk2 gtk3 gtk4 dhcpcd xdg-utils xautolock hostapd xorg-apps dnsmasq rxvt-unicode unzip ppp bluez bluez-untils mathjax youtube-dl python2 python ttf-carlito ttf-caladea ttf-croscore libevent perl xorg-xclock xorg-xmodmap npm nodejs terminus-font mesa mesa-demos qt5ct pwgen imagemagick libjpeg-turbo breeze-icons chromium yajl zip unrar p7zip bzip2 lrzip lz4 lzop xz zstd arj lhasa onboard pulseaudio-bluetooth pulseaudio-equalizer phonon-qt5-vlc opera xf86-input-synaptics bumblebee xf86-video-intel lib32-virtualgl virtualgl bbswitch primus gnome
 
 # Installing main packages from the AUR repository.
-yaourt -S peerflix
+yaourt -S peerflix nvidia-390xx-dkms opencl-nvidia-390xx lib32-opencl-nvidia-390xx lib32-nvidia-390xx-utils nvidia-390xx-utils nvidia-390xx
 
 # Enabling daemons and starting them for my main packages.
 sudo systemctl enable tor.service && sudo systemctl start tor.service
 sudo systemctl enable privoxy.service && sudo systemctl start privoxy.service
 sudo systemctl enable sshd.service && sudo systemctl start sshd.service
 sudo systemctl enable gpm.service && sudo systemctl start gpm.service
-sudo systemctl enable sddm.service && sudo systemctl start sddm.service
 sudo systemctl enable dhcpcd.service && sudo systemctl start dhcpcd.service
 sudo systemctl disable hostapd.service && sudo systemctl stop hostapd.service
 sudo systemctl disable dnsmasq.service && sudo systemctl stop dnsmasq.service
 sudo systemctl enable bluetooth.service && sudo systemctl start bluetooth.service
+sudo systemctl enable bumblebeed.service
 pulseaudio -k && pulseaudio --start
+
+# Adding to groups.
+sudo gpasswd -a $TELLUSER bumblebee
+
+# 
+sudo cat > /etc/modprobe.d/killnouveau.conf <<EOF
+blacklist nouveau
+EOF
 
 # Installing my user.js file in Firefox.
 cd /tmp
