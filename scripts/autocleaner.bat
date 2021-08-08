d44d4d689ab9dff7017fcf8397658a058645f1b5
bogachenko <bogachenkove@gmail.com>
2021-08-09 07:51:41 +1100

    Update autocleaner.bat

--- scripts/autocleaner.bat
+++ scripts/autocleaner.bat
@@ -1,11 +1,20 @@
 @ECHO	off
 :: Edge
-del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Cache\*"
-del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\GPUCache\*"
+RD "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Cache" /S /Q
+RD "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\GPUCache" /S /Q
 :: IE
-del "%LOCALAPPDATA%\Microsoft\Windows\INetCache\*"
+RD "%LOCALAPPDATA%\Microsoft\Windows\INetCache" /S /Q
 :: Firefox
-del "%APPDATA%\Mozilla\Firefox\Profiles\*\formhistory.sqlite"
-del "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\sessionCheckpoints.json"
-del "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\sessionstore-backups\recovery.baklz4"
-del "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\sessionstore-backups\recovery.jsonlz4"
+DEL "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\formhistory.sqlite" /S /Q
+DEL "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\sessionCheckpoints.json" /S /Q
+DEL "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\sessionstore-backups\recovery.baklz4" /S /Q
+DEL "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\sessionstore-backups\recovery.jsonlz4" /S /Q
+:: Windows
+DEL "%WINDIR%\*.log" /S /Q
+RD "%LOCALAPPDATA%\Microsoft\OneDrive\setup\logs" /S /Q
+:: Steam
+RD "%PROGRAMFILES(x86)%\Steam\Logs" /S /Q
+RD "%PROGRAMFILES(x86)%\Steam\Dumps" /S /Q
+DEL "%PROGRAMFILES(x86)%\Steam\*.log" /S /Q
+:: DirectX
+RD "%LOCALAPPDATA%\D3DSCache" /S /Q
