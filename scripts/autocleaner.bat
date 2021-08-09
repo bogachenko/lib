6dda722f6e0d1b16248c1f7c5bbdfed9bb0a6c02
bogachenko <bogachenkove@gmail.com>
2021-08-09 17:49:17 +1100

    Update autocleaner.bat

--- scripts/autocleaner.bat
+++ scripts/autocleaner.bat
@@ -5,10 +5,12 @@ RD "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\GPUCache" /S /Q
 :: IE
 RD "%LOCALAPPDATA%\Microsoft\Windows\INetCache" /S /Q
 :: Firefox
-DEL "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\formhistory.sqlite" /S /Q
-DEL "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\sessionCheckpoints.json" /S /Q
-DEL "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\sessionstore-backups\recovery.baklz4" /S /Q
-DEL "%APPDATA%\Mozilla\Firefox\Profiles\bogachenko\sessionstore-backups\recovery.jsonlz4" /S /Q
+set user=bogachenko
+DEL "%APPDATA%\Mozilla\Firefox\Profiles\%user%\formhistory.sqlite" /S /Q
+DEL "%APPDATA%\Mozilla\Firefox\Profiles\%user%\sessionCheckpoints.json" /S /Q
+DEL "%APPDATA%\Mozilla\Firefox\Profiles\%user%\sessionstore-backups\recovery.baklz4" /S /Q
+DEL "%APPDATA%\Mozilla\Firefox\Profiles\%user%\sessionstore-backups\recovery.jsonlz4" /S /Q
+RD "%ProgramData%\Mozilla"
 :: Windows
 DEL "%WINDIR%\*.log" /S /Q
 RD "%LOCALAPPDATA%\Microsoft\OneDrive\setup\logs" /S /Q
@@ -18,3 +20,11 @@ RD "%PROGRAMFILES(x86)%\Steam\Dumps" /S /Q
 DEL "%PROGRAMFILES(x86)%\Steam\*.log" /S /Q
 :: DirectX
 RD "%LOCALAPPDATA%\D3DSCache" /S /Q
+:: Spotify
+RD "%LOCALAPPDATA%\Spotify\Browser\Cache" /S /Q
+RD "%LOCALAPPDATA%\Spotify\Spotify\Data" /S /Q
+RD "%LOCALAPPDATA%\Spotify\Spotify\Storage" /S /Q
+:: Ace Player
+RD "%SYSTEMDRIVE%\_acestream_cache_" /S /Q
+:: Overwolf
+RD "%ProgramData%\Overwolf\Log" /S /Q
