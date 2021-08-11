c8441fbe3c9fd0b7b52007b15cf0cf0b72647e34
bogachenko <bogachenkove@gmail.com>
2021-08-11 19:06:16 +1100

    Update autosettings.bat

--- scripts/autosettings.bat
+++ scripts/autosettings.bat
@@ -36,6 +36,27 @@ SC delete WalletService
 SC config "NvTelemetryContainer" start=disabled
 SC stop NvTelemetryContainer
 SC delete NvTelemetryContainer
+SC config "WMPNetworkSvc" start=disabled
+SC stop WMPNetworkSvc
+SC delete WMPNetworkSvc
+SC config "wcncsvc" start=disabled
+SC stop wcncsvc
+SC delete wcncsvc
+SC config "SensrSvc" start=disabled
+SC stop SensrSvc
+SC delete SensrSvc
+SC config "WbioSrvc" start=disabled
+SC stop WbioSrvc
+SC delete WbioSrvc
+SC config "RetailDemo" start=disabled
+SC stop RetailDemo
+SC delete RetailDemo
+SC config "SensorService" start=disabled
+SC stop SensorService
+SC delete SensorService
+SC config "SensorDataService" start=disabled
+SC stop SensorDataService
+SC delete SensorDataService
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v "DisableUAR" /t REG_DWORD /d 1 /f
 reg delete "HKLM\SOFTWARE\Policies\Microsoft\SQMClient\Windows" /v "StudyId" /f
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\SQMClient\Windows" /v "CEIPEnable" /t REG_DWORD /d 0 /f
@@ -78,7 +99,6 @@ REG ADD "HKCU\SOFTWARE\Policies\Microsoft\Windows\TabletPC" /v "PreventHandwriti
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\TabletPC" /v "PreventHandwritingDataSharing" /t REG_DWORD /d 1 /f
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows\HandwritingErrorReports" /v "PreventHandwritingErrorReports" /t REG_DWORD /d 1 /f
 REG ADD "HKCU\SOFTWARE\Policies\Microsoft\Windows\HandwritingErrorReports" /v "PreventHandwritingErrorReports" /t REG_DWORD /d 1 /f
-SCHTASKS /Change /TN "Microsoft\Windows\AppID\SmartScreenSpecific" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\Application Experience\ProgramDataUpdater" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\Application Experience\StartupAppTask" /Disable
@@ -86,7 +106,6 @@ SCHTASKS /Change /TN "Microsoft\Windows\Autochk\Proxy" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\CloudExperienceHost\CreateObjectTask" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\Clip\License Validation" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /Disable
-SCHTASKS /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticResolver" /Disable
@@ -97,29 +116,18 @@ SCHTASKS /Change /TN "Microsoft\Windows\Maps\MapsUpdateTask" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\NetTrace\GatherNetworkInfo" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\PI\Sqm-Tasks" /Disable
 SCHTASKS /Change /TN "Microsoft\Windows\SettingSync\NetworkStateChangeTask" /Disable
-SCHTASKS /Change /TN "Microsoft\Windows\SettingSync\BackupTask" /Disable
-SCHTASKS /Change /TN "Microsoft\Windows\WindowsUpdate\Automatic App Update" /Disable
-Call :SetTaskInSystem "Microsoft\Windows\SettingSync\BackgroundUpLoadTask" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\Device Setup\Metadata Refresh" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\HandleCommand" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\HandleWnsCommand" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\IntegrityCheck" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\LocateCommandUserSession" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceAccountChange" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceConnectedToNetwork" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceLocationRightsChange" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\RegisterDevicePeriodic1" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\RegisterDevicePeriodic24" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\RegisterDevicePeriodic6" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\RegisterDevicePolicyChange" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceScreenOnOff" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceSettingChange" "/Disable"
-Call :SetTaskInSystem "Microsoft\Windows\DeviceDirectoryClient\RegisterUserDevice" "/Disable"
-SCHTASKS /Change /TN "\Microsoft\Office\OfficeTelemetryAgentFallBack" /Disable
-SCHTASKS /Change /TN "\Microsoft\Office\OfficeTelemetryAgentLogOn" /Disable
-SCHTASKS /Change /TN "\Microsoft\Office\OfficeTelemetryAgentFallBack2016" /Disable
-SCHTASKS /Change /TN "\Microsoft\Office\OfficeTelemetryAgentLogOn2016" /Disable
-SCHTASKS /Change /TN "\Microsoft\Office\Office 15 Subscription Heartbeat" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\SettingSync\BackgroundUpLoadTask" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\Device Setup\Metadata Refresh" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\DeviceDirectoryClient\HandleCommand" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\DeviceDirectoryClient\HandleWnsCommand" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\DeviceDirectoryClient\IntegrityCheck" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\DeviceDirectoryClient\LocateCommandUserSession" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceAccountChange" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceLocationRightsChange" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\DeviceDirectoryClient\RegisterDevicePeriodic24" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\DeviceDirectoryClient\RegisterDevicePolicyChange" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\DeviceDirectoryClient\RegisterDeviceSettingChange" /Disable
+SCHTASKS /Change /TN "Microsoft\Windows\DeviceDirectoryClient\RegisterUserDevice" /Disable
 REG ADD "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v "Enabled" /t REG_DWORD /d 0 /f
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v "Enabled" /t REG_DWORD /d 0 /f
 REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\AppHost" /v "EnableWebContentEvaluation" /t REG_DWORD /d 0 /f
@@ -128,27 +136,6 @@ REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\SmartGlass" /v "Bluetoot
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\SmartGlass" /v "UserAuthPolicy" /t REG_DWORD /d 0 /f
 REG ADD "HKCU\SOFTWARE\Microsoft\Siuf\Rules" /v "NumberOfSIUFInPeriod" /t REG_DWORD /d 0 /f
 REG ADD "HKCU\SOFTWARE\Microsoft\Siuf\Rules" /v "PeriodInNanoSeconds" /t REG_DWORD /d 0 /f
-SC config "WMPNetworkSvc" start=disabled
-SC stop WMPNetworkSvc
-SC delete WMPNetworkSvc
-SC config "wcncsvc" start=disabled
-SC stop wcncsvc
-SC delete wcncsvc
-SC config "SensrSvc" start=disabled
-SC stop SensrSvc
-SC delete SensrSvc
-SC config "WbioSrvc" start=disabled
-SC stop WbioSrvc
-SC delete WbioSrvc
-SC config "RetailDemo" start=disabled
-SC stop RetailDemo
-SC delete RetailDemo
-SC config "SensorService" start=disabled
-SC stop SensorService
-SC delete SensorService
-SC config "SensorDataService" start=disabled
-SC stop SensorDataService
-SC delete SensorDataService
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Services\LicenseManager" /v "Start" /t REG_DWORD /d 4 /f
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Services\lfsvc" /v "Start" /t REG_DWORD /d 4 /f
 REG ADD "HKLM\SYSTEM\CurrentControlSet\Services\WpnService" /v "Start" /t REG_DWORD /d 4 /f
