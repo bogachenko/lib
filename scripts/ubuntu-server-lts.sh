bef3ee7935decf3b0fd620553bbf0ef4d8a13fcb
bogachenko <bogachenkove@gmail.com>
2021-12-15 00:57:52 +1100

    Create ubuntu-server-lts.sh

---
+++ scripts/ubuntu-server-lts.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Ubuntu Server LTS x86_64
+# Author: Bogachenko Vyacheslav <bogachenkove@gmail.com>
+
+USERNAME=$(whoami)
+
+# Clear the console.
+clear
+
+# Installing updates and updating the data repositories.
+sudo apt update && sudo apt upgrade
+
+# Installing core packages.
+sudo apt-get --no-install-recommends --no-install-suggests install i3 xinit sddm firefox chromium-browser
+
+# Enabling daemons.
+sudo systemctl enable sddm.service && sudo systemctl start sddm.service
+
+# Installing the configuration file user.js for Firefox.
+cd /tmp
+curl -o user.js https://raw.githubusercontent.com/bogachenko/lib/master/mozilla/firefox-user.js
+mv /tmp/user.js ~/.mozilla/firefox/$USERNAME/user.js
+
+# Configuring the Xresources file.
+cat > ~/.Xresources <<EOF
+Xft.dpi: 96
+Xft.antialias: 1
+Xft.hinting: 1
+Xft.rgba: rgb
+Xft.autohint: 0
+Xft.hintstyle: hintslight
+Xft.lcdfilter: lcddefault
+
+URxvt.iso14755: false
+URxvt.iso14755_52: false
+URxvt.clipboard.autocopy: true
+URxvt.keysym.Shift-Control-V: eval:paste_clipboard
+URxvt.keysym.Shift-Control-C: eval:selection_to_clipboard
+URxvt.scrollBar: false
+URxvt.background: #000000
+URxvt.foreground: #ffffff
+URxvt.font: xft:$FONTM:regular:size=9
+URxvt.boldFont: xft:$FONTM:bold:size=9
+URxvt.italicFont: xft:$FONTM:italic:size=9
+URxvt.boldItalicFont: xft:$FONTM:bold:italic:size=9
+URxvt.geometry: 150x30
+URxvt.scrollTtyOutput: false
+URxvt.cursorBlink: true
+EOF
+xrdb -merge ~/.Xresources
