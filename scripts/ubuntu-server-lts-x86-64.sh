f6b7441544778fbcd710f216316921887c949478
bogachenko <bogachenkove@gmail.com>
2022-02-26 18:44:24 +1100

    Update ubuntu-server-lts-x86-64.sh

--- scripts/ubuntu-server-lts-x86-64.sh
+++ scripts/ubuntu-server-lts-x86-64.sh
@@ -22,6 +22,14 @@ APT::Install-Recommends "0";
 APT::Install-Suggests "0";
 EOF
 
+# Installing the VSCodium package
+wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    | gpg --dearmor \
+    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
+    | sudo tee /etc/apt/sources.list.d/vscodium.list
+sudo apt update && sudo apt install codium
+
 # Creating default directories
 xdg-user-dirs-update
 
