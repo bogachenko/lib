438af1911b0da5f43b8269a3160c242189395b69
bogachenko <bogachenkove@gmail.com>
2022-04-24 23:51:50 +1100

    Update windows-x86-64-cleaner.bat

--- scripts/windows-x86-64-cleaner.bat
+++ scripts/windows-x86-64-cleaner.bat
@@ -1,10 +1,19 @@
 @echo off
 
 rem Firefox Browser
-rd "%LOCALAPPDATA%\Mozilla\Firefox\Profiles\%username%\startupCache" /s /q
-del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\formhistory.sqlite" /s /q
-del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\sessionCheckpoints.json" /s /q
-del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\SiteSecurityServiceState.txt" /s /q
 del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\favicons.sqlite" /s /q
 del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\favicons.sqlite-shm" /s /q
 del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\favicons.sqlite-wal" /s /q
+del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\formhistory.sqlite" /s /q
+del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\sessionCheckpoints.json" /s /q
+del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\SiteSecurityServiceState.txt" /s /q
+rd "%APPDATA%\Mozilla\Extensions" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Crash Reports" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Pending Pings" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Profiles\%username%\crashes" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Profiles\%username%\datareporting" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Profiles\%username%\minidumps" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Profiles\%username%\personality-provider" /s /q
+rd "%APPDATA%\Mozilla\SystemExtensionsDev" /s /q
+rd "%LOCALAPPDATA%\Mozilla\Firefox\Profiles\%username%" /s /q
+rd "%ProgramData%\Mozilla" /s /q
