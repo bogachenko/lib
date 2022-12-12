ee6840d218faffc295b25dab96009af8d050088f
bogachenko <bogachenkove@gmail.com>
2022-12-13 02:12:35 +1100

    Update windows-x86-64-cleaner.bat

--- scripts/windows-x86-64-cleaner.bat
+++ scripts/windows-x86-64-cleaner.bat
@@ -55,7 +55,7 @@ del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\favicons.sqlite" /s /q > nul 
 del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\formhistory.sqlite" /s /q > nul 2>&1
 del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\sessionCheckpoints.json" /s /q > nul 2>&1
 del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\SiteSecurityServiceState.txt" /s /q > nul 2>&1
-powershell.exe -command "Remove-item %PROGRAMDATA%\Mozilla-*" -Recurse
+powershell.exe -command "Remove-item '%PROGRAMDATA%\Mozilla-*'" -Recurse
 rd "%APPDATA%\Mozilla\Extensions" /s /q > nul 2>&1
 rd "%APPDATA%\Mozilla\Firefox\Background Tasks Profiles" /s /q > nul 2>&1
 rd "%APPDATA%\Mozilla\Firefox\Crash Reports" /s /q > nul 2>&1
@@ -74,14 +74,28 @@ del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\places.sqlite" /s /q > nul 2>
 del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\places.sqlite-shm" /s /q > nul 2>&1
 del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\places.sqlite-wal" /s /q > nul 2>&1
 rd "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\shader-cache" /s /q > nul 2>&1
-rd "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%sessionstore-backups" /s /q > nul 2>&1
+rd "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\sessionstore-backups" /s /q > nul 2>&1
 
 echo Thunderbird > nul 2>&1
 rd "%LOCALAPPDATA%\Thunderbird\Profiles\%USERNAME%" /s /q > nul 2>&1
 rd "%APPDATA%\Thunderbird\Crash Reports" /s /q > nul 2>&1
 rd "%APPDATA%\Thunderbird\Pending Pings" /s /q > nul 2>&1
 del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\favicons.sqlite" /s /q > nul 2>&1
+del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\favicons.sqlite-shm" /s /q > nul 2>&1
+del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\favicons.sqlite-wal" /s /q > nul 2>&1
 del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\cookies.sqlite" /s /q > nul 2>&1
+del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\cookies.sqlite-shm" /s /q > nul 2>&1
+del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\cookies.sqlite-wal" /s /q > nul 2>&1
+del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\places.sqlite" /s /q > nul 2>&1
+del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\places.sqlite-shm" /s /q > nul 2>&1
+del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\places.sqlite-wal" /s /q > nul 2>&1
+rd "%APPDATA%\Thunderbird\Profiles\%USERNAME%\shader-cache" /s /q > nul 2>&1
+del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\history.sqlite" /s /q > nul 2>&1
+del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\history.sqlite-shm" /s /q > nul 2>&1
+del "%APPDATA%\Thunderbird\Profiles\%USERNAME%\history.sqlite-wal" /s /q > nul 2>&1
+rd "%APPDATA%\Thunderbird\Profiles\%USERNAME%\crashes" /s /q > nul 2>&1
+rd "%APPDATA%\Thunderbird\Profiles\%USERNAME%\minidumps" /s /q > nul 2>&1
+rd "%APPDATA%\Thunderbird\Profiles\%USERNAME%\datareporting" /s /q > nul 2>&1
 
 echo Adobe > nul 2>&1
 rd "%USERPROFILE%\AppData\LocalLow\Adobe" /s /q > nul 2>&1
@@ -94,6 +108,7 @@ echo Discord > nul 2>&1
 rd "%APPDATA%\discord\Cache" /s /q > nul 2>&1
 rd "%APPDATA%\discord\Code Cache" /s /q > nul 2>&1
 rd "%APPDATA%\discord\GPUCache" /s /q > nul 2>&1
+del "%LOCALAPPDATA%\Discord\*.log" /s /q > nul 2>&1
 
 echo Spotify > nul 2>&1
 del "%LOCALAPPDATA%\Spotify\Browser\*.log" /s /q > nul 2>&1
@@ -252,12 +267,24 @@ rd "%PROGRAMDATA%\Adguard\Logs" /s /q > nul 2>&1
 rd "%PROGRAMDATA%\Adguard\Temp" /s /q > nul 2>&1
 rd "%PROGRAMDATA%\Adguard\Crashes" /s /q > nul 2>&1
 
+echo qBittorrent > nul 2>&1
+rd "%LOCALAPPDATA%\qBittorrent" /s /q > nul 2>&1
+
+echo Rockstar Games Launcher > nul 2>&1
+rd "%LOCALAPPDATA%\Rockstar Games\Launcher\CrashLogs" /s /q > nul 2>&1
+
+echo Squirrel > nul 2>&1
+rd "%LOCALAPPDATA%\SquirrelTemp" /s /q > nul 2>&1
+
+echo GitHub Desktop > nul 2>&1
+rd "%LOCALAPPDATA%\GitHubDesktop\*.log" /s /q > nul 2>&1
+
 echo Windows > nul 2>&1
 del "%LOCALAPPDATA%\Microsoft\Windows\Explorer\*.db" /s /q > nul 2>&1
 del "%PROGRAMDATA%\Microsoft\Search\Data\Applications\Windows\*.*" /s /q > nul 2>&1
 del "%WINDIR%\*.log" /s /q > nul 2>&1
-powershell.exe -command "Remove-item %LOCALAPPDATA%\Microsoft\CLR_*" -Recurse
-powershell.exe -command "Remove-item %WINDIR%\System32\sru\SRU*.*" -Recurse
+powershell.exe -command "Remove-item '%LOCALAPPDATA%\Microsoft\CLR_*'" -Recurse
+powershell.exe -command "Remove-item '%WINDIR%\System32\sru\SRU*.*'" -Recurse
 rd "%APPDATA%\Microsoft\Windows\Recent" /s /q > nul 2>&1
 rd "%LOCALAPPDATA%\ElevatedDiagnostics" /s /q > nul 2>&1
 rd "%LOCALAPPDATA%\Microsoft\Windows\WebCache" /s /q > nul 2>&1
@@ -277,6 +304,8 @@ rd "%LOCALAPPDATA%\Microsoft\Windows\Caches" /s /q > nul 2>&1
 rd "%LOCALAPPDATA%\Microsoft\Windows\Temporary Internet Files" /s /q > nul 2>&1
 rd "%WINDIR%\Offline Web Pages" /s /q > nul 2>&1
 rd "%PROGRAMDATA%\Microsoft\Windows\WER" /s /q > nul 2>&1
+del "%LOCALAPPDATA%\IconCache.db" /s /q > nul 2>&1
+rd "%LOCALAPPDATA%\cache" /s /q > nul 2>&1
 
 echo Notepad++ > nul 2>&1
 del "%APPDATA%\notepad++\session.xml" /s /q > nul 2>&1
@@ -285,9 +314,13 @@ echo Nvidia > nul 2>&1
 rd "%LOCALAPPDATA%\NVIDIA\DXCache" /s /q > nul 2>&1
 rd "%LOCALAPPDATA%\NVIDIA\GLCache" /s /q > nul 2>&1
 
+echo IObit Driver Booster > nul 2>&1
+rd "%APPDATA%\IObit\Driver Booster\Logs" /s /q > nul 2>&1
+
 echo Telegram > nul 2>&1
 del "%APPDATA%\Telegram Desktop\log.txt" /s /q > nul 2>&1
-rd "%APPDATA%\\Telegram Desktop\tdata\dumps" /s /q > nul 2>&1
+powershell.exe -command "Remove-item '%APPDATA%\Telegram Desktop\log_*.txt'" -Recurse
+rd "%APPDATA%\Telegram Desktop\tdata\dumps" /s /q > nul 2>&1
 rd "%APPDATA%\Telegram Desktop\tdata\user_data" /s /q > nul 2>&1
 
 echo Countdown > nul 2>&1
