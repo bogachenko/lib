06866f54eeb878d47c391cca69e721a8cb2f6fea
bogachenko <bogachenkove@gmail.com>
2022-12-12 14:51:26 +1100

    Update windows-x86-64-cleaner.bat

--- scripts/windows-x86-64-cleaner.bat
+++ scripts/windows-x86-64-cleaner.bat
@@ -16,24 +16,24 @@ rem Flush DNS Cache
 ipconfig /flushdns
 
 rem Firefox Browser
-del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\favicons.sqlite" /s /q
-del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\favicons.sqlite-shm" /s /q
-del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\favicons.sqlite-wal" /s /q
-del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\formhistory.sqlite" /s /q
-del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\places.sqlite" /s /q
-del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\sessionCheckpoints.json" /s /q
-del "%APPDATA%\Mozilla\Firefox\Profiles\%username%\SiteSecurityServiceState.txt" /s /q
-rd "%APPDATA%\Mozilla\Firefox\Background Tasks Profiles" /s /q
+del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\favicons.sqlite-shm" /s /q
+del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\favicons.sqlite-wal" /s /q
+del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\favicons.sqlite" /s /q
+del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\formhistory.sqlite" /s /q
+del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\places.sqlite" /s /q
+del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\sessionCheckpoints.json" /s /q
+del "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\SiteSecurityServiceState.txt" /s /q
+powershell.exe -command "Remove-item %ProgramData%\Mozilla-*" -Recurse
 rd "%APPDATA%\Mozilla\Extensions" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Background Tasks Profiles" /s /q
 rd "%APPDATA%\Mozilla\Firefox\Crash Reports" /s /q
 rd "%APPDATA%\Mozilla\Firefox\Pending Pings" /s /q
-rd "%APPDATA%\Mozilla\Firefox\Profiles\%username%\crashes" /s /q
-rd "%APPDATA%\Mozilla\Firefox\Profiles\%username%\datareporting" /s /q
-rd "%APPDATA%\Mozilla\Firefox\Profiles\%username%\minidumps" /s /q
-rd "%APPDATA%\Mozilla\Firefox\Profiles\%username%\personality-provider" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\crashes" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\datareporting" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\minidumps" /s /q
+rd "%APPDATA%\Mozilla\Firefox\Profiles\%USERNAME%\personality-provider" /s /q
 rd "%APPDATA%\Mozilla\SystemExtensionsDev" /s /q
-rd "%LOCALAPPDATA%\Mozilla\Firefox\Profiles\%username%" /s /q
-powershell.exe -command "Remove-item %ProgramData%\Mozilla-*" -Recurse
+rd "%LOCALAPPDATA%\Mozilla\Firefox\Profiles\%USERNAME%" /s /q
 rd "%USERPROFILE%\AppData\LocalLow\Mozilla" /s /q
 
 rem Adobe
@@ -58,38 +58,41 @@ rd "%LOCALAPPDATA%\Spotify\Storage" /s /q
 
 rem Steam
 del "%PROGRAMFILES(x86)%\Steam\*.log" /s /q
+rd "%LOCALAPPDATA%\Steam\htmlcache" /s /q
 rd "%PROGRAMFILES(x86)%\Steam\dumps" /s /q
 rd "%PROGRAMFILES(x86)%\Steam\logs" /s /q
-rd "%LOCALAPPDATA%\Steam\htmlcache" /s /q
+
+rem Skype
+rd "%APPDATA%\Microsoft\Skype for Desktop\GPUCache" /s /q
 
 rem Opera Browser
 del "%APPDATA%\Opera Software\Opera Stable\*.log" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\Cookies" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\Cookies-journal" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\Cookies" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\default_partner_content.json" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\Extension Cookies" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\Extension Cookies-journal" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\Favicons" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\Extension Cookies" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\Favicons-journal" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\History" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\History-journal" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\Favicons" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\History Provider Cache" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\Media History" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\History-journal" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\History" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\Media History-journal" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\Network Action Predictor" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\Media History" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\Network Action Predictor-journal" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\Network Persistent State" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\Network Action Predictor" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\Network Persistent State.backup" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\Network Persistent State" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\PartnerRules" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\QuotaManager" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\QuotaManager-journal" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\Reporting and NEL" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\QuotaManager" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\Reporting and NEL-journal" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\Shortcuts" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\Reporting and NEL" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\Shortcuts-journal" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\Shortcuts" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\Visited Links" /s /q
-del "%APPDATA%\Opera Software\Opera Stable\Web Data" /s /q
 del "%APPDATA%\Opera Software\Opera Stable\Web Data-journal" /s /q
+del "%APPDATA%\Opera Software\Opera Stable\Web Data" /s /q
 rd "%APPDATA%\Opera Software\Opera Stable\Crash Reports" /s /q
 rd "%APPDATA%\Opera Software\Opera Stable\databases" /s /q
 rd "%APPDATA%\Opera Software\Opera Stable\Extension Rules" /s /q
@@ -103,8 +106,8 @@ rd "%APPDATA%\Opera Software\Opera Stable\Jump List IconsOld" /s /q
 rd "%APPDATA%\Opera Software\Opera Stable\Local Storage" /s /q
 rd "%APPDATA%\Opera Software\Opera Stable\SafetyTips" /s /q
 rd "%APPDATA%\Opera Software\Opera Stable\Service Worker" /s /q
-rd "%APPDATA%\Opera Software\Opera Stable\Sessions" /s /q
 rd "%APPDATA%\Opera Software\Opera Stable\Session Storage" /s /q
+rd "%APPDATA%\Opera Software\Opera Stable\Sessions" /s /q
 rd "%APPDATA%\Opera Software\Opera Stable\ShaderCache" /s /q
 rd "%APPDATA%\Opera Software\Opera Stable\shared_proto_db" /s /q
 rd "%APPDATA%\Opera Software\Opera Stable\StatsSessions" /s /q
@@ -113,17 +116,17 @@ rd "%LOCALAPPDATA%\Opera Software\Opera Stable" /s /q
 rem Edge Browser
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\BrowserMetrics-*" /s /q
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\CrashpadMetrics-active.pma" /s /q
-del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Favicons" /s /q
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Favicons-journal" /s /q
-del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\heavy_ad_intervention_opt_out.db" /s /q
+del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Favicons" /s /q
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\heavy_ad_intervention_opt_out.db-journal" /s /q
-del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\History" /s /q
-del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\History-journal" /s /q
+del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\heavy_ad_intervention_opt_out.db" /s /q
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\History Provider Cache" /s /q
+del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\History-journal" /s /q
+del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\History" /s /q
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\load_statistics.db" /s /q
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Network Action Predictor" /s /q
-del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Shortcuts" /s /q
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Shortcuts-journal" /s /q
+del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Shortcuts" /s /q
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Storage" /s /q
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Top Sites" /s /q
 del "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Visited Links" /s /q
@@ -144,8 +147,8 @@ rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\optimization_guide_model_and
 rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Platform Notifications" /s /q
 rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Safe Browsing Network" /s /q
 rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Service Worker" /s /q
-rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Sessions" /s /q
 rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Session Storage" /s /q
+rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Sessions" /s /q
 rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\shared_proto_db" /s /q
 rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\Site Characteristics Database" /s /q
 rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\Default\VideoDecodeStats" /s /q
@@ -154,27 +157,28 @@ rd "%LOCALAPPDATA%\Microsoft\Edge\User Data\ShaderCache" /s /q
 
 rem Chromium
 del "%LOCALAPPDATA%\Chromium\User Data\BrowserMetrics-*" /s /q
-del "%LOCALAPPDATA%\Chromium\User Data\Default\Favicons" /s /q
 del "%LOCALAPPDATA%\Chromium\User Data\Default\Favicons-journal" /s /q
-del "%LOCALAPPDATA%\Chromium\User Data\Default\heavy_ad_intervention_opt_out.db" /s /q
+del "%LOCALAPPDATA%\Chromium\User Data\Default\Favicons" /s /q
 del "%LOCALAPPDATA%\Chromium\User Data\Default\heavy_ad_intervention_opt_out.db-journal" /s /q
-del "%LOCALAPPDATA%\Chromium\User Data\Default\History" /s /q
+del "%LOCALAPPDATA%\Chromium\User Data\Default\heavy_ad_intervention_opt_out.db" /s /q
 del "%LOCALAPPDATA%\Chromium\User Data\Default\History-journal" /s /q
-del "%LOCALAPPDATA%\Chromium\User Data\Default\Network Action Predictor" /s /q
+del "%LOCALAPPDATA%\Chromium\User Data\Default\History" /s /q
 del "%LOCALAPPDATA%\Chromium\User Data\Default\Network Action Predictor-journal" /s /q
-del "%LOCALAPPDATA%\Chromium\User Data\Default\Shortcuts" /s /q
+del "%LOCALAPPDATA%\Chromium\User Data\Default\Network Action Predictor" /s /q
 del "%LOCALAPPDATA%\Chromium\User Data\Default\Shortcuts-journal" /s /q
-del "%LOCALAPPDATA%\Chromium\User Data\Default\Top Sites" /s /q
+del "%LOCALAPPDATA%\Chromium\User Data\Default\Shortcuts" /s /q
 del "%LOCALAPPDATA%\Chromium\User Data\Default\Top Sites-journal" /s /q
+del "%LOCALAPPDATA%\Chromium\User Data\Default\Top Sites" /s /q
 del "%LOCALAPPDATA%\Chromium\User Data\Default\Visited Links" /s /q
-del "%LOCALAPPDATA%\Chromium\User Data\Default\Web Data" /s /q
 del "%LOCALAPPDATA%\Chromium\User Data\Default\Web Data-journal" /s /q
+del "%LOCALAPPDATA%\Chromium\User Data\Default\Web Data" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\BrowserMetrics" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\Cache" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\commerce_subscription_db" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\coupon_db" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\databases" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\DawnCache" /s /q
+rd "%LOCALAPPDATA%\Chromium\User Data\Default\Extension State" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\Feature Engagement Tracker" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\GPUCache" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\IndexedDB" /s /q
@@ -183,13 +187,12 @@ rd "%LOCALAPPDATA%\Chromium\User Data\Default\Network" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\optimization_guide_hint_cache_store" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\optimization_guide_model_metadata_store" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\Platform Notifications" /s /q
-rd "%LOCALAPPDATA%\Chromium\User Data\Default\Sessions" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\Session Storage" /s /q
+rd "%LOCALAPPDATA%\Chromium\User Data\Default\Sessions" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\shared_proto_db" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\Site Characteristics Database" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\VideoDecodeStats" /s /q
 rd "%LOCALAPPDATA%\Chromium\User Data\Default\WebrtcVideoStats" /s /q
-rd "%LOCALAPPDATA%\Chromium\User Data\Default\Extension State" /s /q
 
 rem AdGuard VPN
 rd "%ProgramData%\AdguardVpn\Logs" /s /q
@@ -198,25 +201,26 @@ rem Overwolf
 rd "%ProgramData%\Overwolf\Log" /s /q
 
 rem Windows
-rd "%SYSTEMDRIVE%\$Recycle.bin" /s /q
+del "%LOCALAPPDATA%\Microsoft\Windows\Explorer\*.db" /s /q
+del "%ProgramData%\Microsoft\Search\Data\Applications\Windows\*.*" /s /q
 del "%WINDIR%\*.log" /s /q
 del "%WINDIR%\Temp\" /s /q
-del "%LOCALAPPDATA%\Microsoft\Windows\Explorer\*.db" /s /q
-rd "%ProgramData%\Microsoft\Diagnosis" /s /q
-rd "%WINDIR%\Logs" /s /q
-powershell.exe -command "Remove-item %WINDIR%\System32\sru\SRU*.*" -Recurse
-rd "%LOCALAPPDATA%\Temp" /s /q
-rd "%WINDIR%\Installer\$PatchCache$\Managed" /s /q
-rd "%LOCALAPPDATA%\Microsoft\Windows\WebCache" /s /q
 powershell.exe -command "Remove-item %LOCALAPPDATA%\Microsoft\CLR_*" -Recurse
+powershell.exe -command "Remove-item %WINDIR%\System32\sru\SRU*.*" -Recurse
 rd "%APPDATA%\Microsoft\Windows\Recent" /s /q
-rd "%USERPROFILE%\AppData\LocalLow\Microsoft\CryptnetUrlCache" /s /q
-rd "%USERPROFILE%\AppData\LocalLow\\Microsoft\Internet Explorer" /s /q
+rd "%LOCALAPPDATA%\ElevatedDiagnostics" /s /q
+rd "%LOCALAPPDATA%\Microsoft\Windows\WebCache" /s /q
+rd "%LOCALAPPDATA%\Temp" /s /q
+rd "%ProgramData%\Microsoft\Diagnosis" /s /q
 rd "%ProgramData%\Microsoft\Windows Defender" /s /q
-rd "%WINDIR%\SoftwareDistribution" /s /q
-del "%ProgramData%\Microsoft\Search\Data\Applications\Windows\*.*" /s /q
-rd "%WINDIR%\Prefetch" /s /q
+rd "%SYSTEMDRIVE%\$Recycle.bin" /s /q
+rd "%USERPROFILE%\AppData\LocalLow\\Microsoft\Internet Explorer" /s /q
+rd "%USERPROFILE%\AppData\LocalLow\Microsoft\CryptnetUrlCache" /s /q
 rd "%WINDIR%\DiagTrack" /s /q
+rd "%WINDIR%\Installer\$PatchCache$\Managed" /s /q
+rd "%WINDIR%\Logs" /s /q
+rd "%WINDIR%\Prefetch" /s /q
+rd "%WINDIR%\SoftwareDistribution" /s /q
 
 rem Notepad++
 del "%APPDATA%\notepad++\session.xml" /s /q
@@ -226,9 +230,9 @@ rd "%LOCALAPPDATA%\NVIDIA\DXCache" /s /q
 rd "%LOCALAPPDATA%\NVIDIA\GLCache" /s /q
 
 rem Telegram
-rd "%APPDATA%\Telegram Desktop\tdata\user_data" /s /q
-rd "%APPDATA%\\Telegram Desktop\tdata\dumps" /s /q
 del "%APPDATA%\Telegram Desktop\log.txt" /s /q
+rd "%APPDATA%\\Telegram Desktop\tdata\dumps" /s /q
+rd "%APPDATA%\Telegram Desktop\tdata\user_data" /s /q
 
 rem Countdown
 timeout 5
