48fd72026ba1350bf80c95e3feaf93496453552e
bogachenko <bogachenkove@gmail.com>
2023-06-02 01:07:10 +1100

    Update archlinux-aarch64.sh

--- scripts/archlinux-aarch64.sh
+++ scripts/archlinux-aarch64.sh
@@ -8,6 +8,7 @@ sudo sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoe
 #USERNAME="$(whoami)"
 #MYDIR="/home/username/"
 
+echo 'Updating the list of repository mirrors.'
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 cat > /tmp/mirrorlist <<EOF
 Server = http://mirror.yandex.ru/archlinux-arm/$arch/$repo
@@ -31,7 +32,7 @@ sudo pacman -S --needed --noconfirm git vim wget alsa-plugins alsa-utils pulseau
 echo 'Installing the extra packages.'
 sudo pacman -S --needed --noconfirm chromium firefox tor vlc zip unrar p7zip bzip2 arj lrzip lz4 lzop xz zstd yt-dlp unzip sddm i3-wm i3status i3lock i3blocks dunst rofi dmenu rxvt-unicode plymouth
 
-echo ''
+echo 'Setting preferences for kernel parameters.'
 sudo sed -i 's/HOOKS=\(base udev autodetect modconf kms keyboard keymap consolefont block filesystems fsck\)/HOOKS=\(base udev autodetect modconf kms keyboard keymap plymouth consolefont block filesystems fsck\)/g' /etc/mkinitcpio.conf
 
 echo 'Installing additional repositories.'
@@ -76,16 +77,17 @@ sudo sed -i -e "s/server 1.arch.pool.ntp.org/server 1.ru.pool.ntp.org/g" /etc/nt
 sudo sed -i -e "s/server 2.arch.pool.ntp.org/server 2.ru.pool.ntp.org/g" /etc/ntp.conf
 sudo sed -i -e "s/server 3.arch.pool.ntp.org/server 3.ru.pool.ntp.org/g" /etc/ntp.conf
 
-echo 'Subpixel hinting mode.'
+echo 'Setting preferences for fonts.'
 sudo sed -i 's/#export FREETYPE_PROPERTIES/export FREETYPE_PROPERTIES/g' /etc/profile.d/freetype2.sh
 
 echo ''
 sudo hostnamectl set-hostname raspberry
 sudo useradd -m -g users -G wheel -s /bin/zsh username
 sudo passwd username
+sudo userdel -rf alarm
 sudo chsh -s /bin/zsh root
 
-echo ''
+echo 'Setting preferences for automatic login to SDDM.'
 cat > /tmp/etc/sddm.conf <<EOF
 [Autologin]
 User=username
@@ -93,13 +95,13 @@ Session=i3.desktop
 EOF
 sudo mv /tmp/etc/sddm.conf /etc/sddm.conf
 
-echo ''
+echo 'Setting preferences for Privoxy.'
 sudo sh -c "echo \"forward-socks5 / localhost:9050 .\" >> /etc/privoxy/config"
 sudo sh -c "echo \"forward-socks4 / localhost:9050 .\" >> /etc/privoxy/config"
 sudo sh -c "echo \"forward-socks4a / localhost:9050 .\" >> /etc/privoxy/config"
 sudo systemctl restart privoxy.service
 
-echo ''
+echo 'Setting preferences for the language.'
 sudo localectl set-keymap en
 sudo setfont cyr-sun16
 sudo localectl set-locale LANG="en_US.UTF-8"
@@ -112,7 +114,7 @@ EOF
 sudo mv /tmp/locale.gen /etc/locale.gen
 sudo locale-gen
 
-echo ''
+echo 'Setting preferences for working directories.'
 # Creating dirictory.
 cd ~
 mkdir '~/.config'
@@ -131,7 +133,7 @@ echo "exec_always --no-startup-id xsetroot -solid \"#003760\"" >> /home/username
 echo "set \$mod Mod4" >> /home/username/.config/i3/config
 echo "exec i3" >> /home/username/.xinitrc
 
-echo 'Setting preferences for .zsh'
+echo 'Setting preferences for Z shell'
 cat > /tmp/.zshrc <<EOF
 PROMPT="%F{34}%n%f%F{34}@%f%F{34}%m%f:%F{21}%~%f$ "
 export BROWSER="firefox"
