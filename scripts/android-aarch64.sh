f5f15e9ecd79666e0505a563e7260232a0aa99cf
bogachenko <bogachenkove@gmail.com>
2023-12-02 18:52:50 +1100

    Update files

--- scripts/android-aarch64.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Android aarch64
-# Author: Bogachenko Vyacheslav <bogachenkove@gmail.com>
-
-pkg install root-repo x11-repo
-pkg upgrade
-termux-change-repo
-pkg install --no-install-recommends tor vim iw htop cmake make git speedtest-go php openssh python tsu ruby wget curl hwinfo termux-api termux-tools termux-auth
