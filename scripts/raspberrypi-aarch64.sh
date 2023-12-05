abb79a6e7858318813caa2385c2475e49619b4a8
bogachenko <bogachenkove@gmail.com>
2023-12-05 19:02:05 +1100

    Update files

--- scripts/raspberrypi-aarch64.sh
+++ scripts/raspberrypi-aarch64.sh
@@ -10,7 +10,7 @@ echo 'Removing garbage packages after updating packages.'
 sudo apt autoremove
 
 echo 'Installing the core packages.'
-sudo apt install --no-install-recommends --no-install-suggests openssh-server xserver-xorg x11-utils x11-xserver-utils xfonts-base xterm console-cyrillic htop python3 python3-pip xinit mesa-utils zsh ufw net-tools dialog ifplugd netctl perl ruby php gpm apparmor xdg-utils xss-lock systemd-resolved
+sudo apt install --no-install-recommends --no-install-suggests openssh-server xserver-xorg x11-utils x11-xserver-utils xfonts-base xterm console-cyrillic htop python3 python3-pip xinit mesa-utils zsh ufw net-tools dialog ifplugd netctl perl ruby php gpm apache2 apparmor xdg-utils xss-lock systemd-resolved
 curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
 echo 'Installing the sub-core packages.'
 sudo apt install --no-install-recommends --no-install-suggests vim git pwgen wireplumber pipewire pipewire-jack pipewire-alsa pipewire-pulse alsa-utils ffmpeg mpd ranger zip unrar p7zip unzip wget lshw dnsmasq hostapd tor nyx privoxy fonts-ubuntu fonts-noto-color-emoji fonts-noto-mono fonts-noto fonts-liberation2 fonts-dejavu
@@ -25,6 +25,7 @@ DNS=127.0.0.1
 DNSStubListener=no
 EOF
 sudo mv /etc/resolv.conf /etc/resolv.conf.backup
+sudo sed -i 's/#DNS=/DNS=1.1.1.1 1.0.0.1/g' /etc/systemd/resolved.conf
 sudo ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 sudo systemctl restart systemd-resolved.service
 
@@ -41,6 +42,7 @@ sudo systemctl enable gpm.service
 sudo systemctl enable bluetooth.service
 sudo systemctl enable lightdm.service
 sudo systemctl enable ifplugd.service
+sudo systemctl enable apache2
 sudo raspi-config
 
 echo 'Enabling the Z shell by default.'
@@ -52,12 +54,15 @@ sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow 22/tcp
 sudo ufw allow 21/tcp
+sudo ufw allow 631/tcp
 sudo ufw allow 9050/tcp
 sudo ufw allow 3000/tcp
+sudo ufw allow 5355
 sudo ufw allow 853
 sudo ufw allow 443
 sudo ufw allow 80
 sudo ufw allow 53
+sudo ufw allow 8118
 sudo ufw allow 67/udp
 sudo ufw allow 68/udp
 sudo ufw enable
