b28de6d23dcf1b12ba5b6b7f12b018c11d537e30
bogachenko <bogachenkove@gmail.com>
2023-12-05 14:04:54 +1100

    Update raspberrypi-aarch64.sh

--- scripts/raspberrypi-aarch64.sh
+++ scripts/raspberrypi-aarch64.sh
@@ -3,34 +3,31 @@
 # Raspberry Pi aarch64
 # Author: Bogachenko Vyacheslav <bogachenkove@gmail.com>
 
-sudo useradd -m -g users -G wheel -s /bin/zsh username
-
-echo 'Обновление списка пакетов'
+echo 'Updating the package list.'
 sudo apt update; sudo apt upgrade
 
-echo 'Удаление мусорных пакетов'
+echo 'Removing garbage packages after updating packages.'
 sudo apt autoremove
 
-sudo snap disable firefox
-sudo snap remove --purge firefox
-sudo add-apt-repository ppa:mozillateam/ppa
-
-echo 'Установка приложений'
-sudo apt install --no-install-recommends --no-install-suggests openssh-server xserver-xorg x11-utils x11-xserver-utils xfonts-base xterm console-cyrillic htop python3 python3-pip xinit mesa-utils zsh ufw ntp net-tools perl ruby php gpm apparmor xdg-utils xss-lock
+echo 'Installing the core packages.'
+sudo apt install --no-install-recommends --no-install-suggests openssh-server xserver-xorg x11-utils x11-xserver-utils xfonts-base xterm console-cyrillic htop python3 python3-pip xinit mesa-utils zsh ufw net-tools perl ruby php gpm apparmor xdg-utils xss-lock
+curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
+echo 'Installing the sub-core packages.'
 sudo apt install --no-install-recommends --no-install-suggests vim git pipewire pipewire-jack pipewire-alsa pipewire-pulse alsa-utils rofi i3 sddm dunst scrot rxvt-unicode mpd ranger zip unrar p7zip unzip wget lshw
+echo 'Installing the extra packages.'
 sudo apt install --no-install-recommends --no-install-suggests tor firefox plymouth
 
-echo 'Включение сервисов'
+echo 'Enabling and running services.'
 sudo systemctl enable ssh.service
 sudo systemctl enable ufw.service
-sudo systemctl enable ntpsec.service
 sudo systemctl enable tor.service
 sudo systemctl enable mpd.service
 
+echo 'Enabling the Z shell by default.'
 sudo chsh -s /bin/zsh root
 sudo chsh -s /bin/zsh username
 
-echo 'Uncomplicated Firewall Rules'
+echo 'Enabling firewall rules.'
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow 123/udp
@@ -40,12 +37,3 @@ sudo ufw allow 5900/tcp
 sudo ufw allow 443
 sudo ufw allow 80
 sudo ufw enable
-
-echo 'AdGuard Home'
-curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
-
-sudo sed -i -e "s/pool 0.debian.pool.ntp.org/pool 0.ru.pool.ntp.org/g" /etc/ntpsec/ntp.conf
-sudo sed -i -e "s/pool 1.debian.pool.ntp.org/pool 1.ru.pool.ntp.org/g" /etc/ntpsec/ntp.conf
-sudo sed -i -e "s/pool 2.debian.pool.ntp.org/pool 2.ru.pool.ntp.org/g" /etc/ntpsec/ntp.conf
-sudo sed -i -e "s/pool 3.debian.pool.ntp.org/pool 3.ru.pool.ntp.org/g" /etc/ntpsec/ntp.conf
-sudo systemctl restart ntpsec.service
