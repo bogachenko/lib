3f4344fcd0ec6309e71707fe1828a4439fa6a51a
bogachenko <bogachenkove@gmail.com>
2024-08-06 23:09:07 +0300

    Update files

--- scripts/windows-enterprise-ltsc-x86-64.bat
+++ scripts/windows-enterprise-ltsc-x86-64.bat
@@ -4,24 +4,24 @@ title Windows 10 Enterprise LTSC
 :: Windows 10 Enterprise LTSC build 19044.4529 x86_64
 :: Author: Bogachenko Vyacheslav <bogachenkove@gmail.com>
 :: License: MIT license <https://raw.githubusercontent.com/bogachenko/lib/master/LICENSE.md>
-:: Last update: July 2024
+:: Last update: August 2024
 :: Donate:
 ::  Bitcoin (BTC) - bc1qnmderzmag45pa3a0jyaqah654x0055n6y8madh
 ::  Bitcoin Cash (BCH) - qzptqmjr0vrj053wgcxa5yesdlejk973xq9xctmxcg
+::  Binance Coin (BNB) 0xE4869C3140134522383E316d329612bC886F85E2
 ::  Ethereum (ETH) - 0xE4869C3140134522383E316d329612bC886F85E2
 ::  Ethereum Classic (ETC) - 0x4A11511a9C69eA1CFa8B1135c9B8A3B27c84eE55
 ::  Tron (TRX) - TW8ocDJLPTnVFG4Karb7zctbBfjFaZfuJn
-::  Toncoin (TON) - UQCt3JyjW8EvFidU18qnrnfMnSdTWdqKJkJpvuxW9W0dwTU7
+::  Toncoin (TON) - UQA5s93oUhxmmkaivrZim1VOh9v-D6CI15jlk80FP6wWtYrw
 ::  Dogecoin (DOGE) - D7BHKJ4ymU5uZKrju5BbYQpSfdENc5qtr1
 ::  Litecoin (LTC) - ltc1q3t9hmgqyze8qlrw56rxepyw8ll44jcl7uc8z4p
 ::  Solana (SOL) - 5fsRA5NiQKX5GLzatbmYS7UbZ9Q2LMqdCH7pBgtrXDJM
 ::  Ripple (XRP) - rnEWArfEDm4yFJeG7xnvDCkW7GKperxf6t
+::  Dash (DASH) - XkQFy1UfKCCpiSw391A5YYTEYEKYvxVoxE
 ::  Cardano (ADA) - addr1q8yjcner4yq7kgd0gleq4qf0gae2xemvvpu790nhfk7a3y8gje4zxphcq0kyg3ry5yvgtzy2huhd49l9rdwmh6khmm4se2er3a
 ::  Zcash (ZEC) - t1N8QRJg6jFt2m92DyFkYVDEv36ZK3JnQP2
-::  Binance Coin (BNB) uses the ETH address.
 ::  Tether (USDT) uses TRX, ETH, BNB, TON or SOL addresses, depending on the type of chain chosen.
 ::  USD Coin (USDC) uses TRX, ETH, BNB or SOL addresses, depending on the type of chain chosen.
-::  Binance USD (BUSD) uses TRX, ETH, BNB addresses, depending on the type of chain chosen.
 
 echo NETWORK INFORMATION RETRIEVAL.
 timeout /t "1" /nobreak >nul
